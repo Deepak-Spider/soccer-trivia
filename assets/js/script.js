4eace549354235c5e02fe7acf5a62fc18f8bbdb0
deepakdev <deepakp.tvla@gmail.com>
2023-12-24 22:55:53 +0000

    Add functions for user-selected answers, validating if all questions are answered

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,80 +76,119 @@ function saveUserName() {
 }
 
 // Array of quiz questions
-let questions = [
+const questions = [
     {
+        questionID: 0,
         question: 'Which team holds the record for most FIFA World Cup titles?',
-        choice1: 'Germany',
-        choice2: 'Brazil',
-        choice3: 'Italy',
-        choice4: 'Argentina',
-        answer: 2,
+        answers: {
+            a: 'Germany', 
+            b: 'Brazil',
+            c: 'Italy',
+            d: 'Argentina'
+        }, 
+        correctAnswer: 'b'
+    }, 
+    {
+        questionID: 1,
+        question: 'How many minutes is a standard soccer match?',
+        answers: {
+            a: '90 minutes', 
+            b: '120 minutes',
+            c: '60 minutes', 
+            d: '75 minutes'
+        }, 
+        correctAnswer: 'a'
     },
     {
-        question:"How many minutes is a standard soccer match?",
-        choice1: "90 minutes",
-        choice2: "120 minutes",
-        choice3: "60 minutes",
-        choice4: "75 minutes",
-        answer: 1,
+        questionID: 2,
+        question: 'Who is known as the "Hand of God" for his infamous goal in the 1986 World Cup?',
+        answers: {
+            a: 'Diego Maradona', 
+            b: 'Zinedine Zidane',
+            c: 'Ronaldo Nazario', 
+            d: 'Thierry Henry'
+        }, 
+        correctAnswer: 'a'
     },
     {
-        question: "Who is known as the Hand of God for his infamous goal in the 1986 World Cup?",
-        choice1: "Diego Maradona",
-        choice2: "Zinedine Zidane",
-        choice3: "Ronaldo Nazario",
-        choice4: "Thierry Henry",
-        answer: 1,
+        questionID: 3,
+        question: 'What is the name of the designated area where a penalty kick is taken?',
+        answers: {
+            a: 'Penalty box', 
+            b: 'Penalty area',
+            c: 'Penalty spot',
+            d: 'Penalty circle'
+        }, 
+        correctAnswer: 'a'
     },
     {
-        question:"What is the name of the designated area where a penalty kick is taken?",
-        choice1: "Penalty box",
-        choice2: "Penalty area",
-        choice3: "Penalty spot",
-        choice4: "Penalty circle",
-        answer: 1,
+        questionID: 4,
+        question: ' What is the name of the award given to the player who scores the most goals in a season?',
+        answers: {
+            a: 'Golden Boot',
+            b: 'Golden Ball',
+            c: 'Sliver Boot',
+            d: 'Bronze Boot)'
+        },
+        correctAnswer: 'a'
+    },
+
+    {
+        questionID: 5,
+        question: 'How many players are there on a standard soccer team during a match?',
+        answers: {
+            a: '9',
+            b: '10',
+            c: '11',
+            d: '12'
+        },
+        correctAnswer: 'c'
     },
     {
-        question: 'What is the name of the award given to the player who scores the most goals in a season?',
-        choice1: 'Golden Boot',
-        choice2: 'Golden Ball',
-        choice3: 'Silver Boot',
-        choice4: 'Bronze Boot',
-        answer: 1,
+        questionID: 6,
+        question: ' Which club has won the most UEFA Champions League titles until December 2023?',
+        answers: {
+            a: 'Real Madrid',
+            b: 'FC Barcelona',
+            c: 'AC Milan',
+            d: 'Bayern Munich'
+        },
+        correctAnswer: 'a'
     },
-    {
-        question: 'Which club has won the most UEFA Champions League titles until December 2023?',
-        choice1: 'Real Madrid',
-        choice2: 'FC Barcelona',
-        choice3: 'AC Milan',
-        choice4: 'Bayern Munich',
-        answer: 1,
-    },
-    {
+    { 
+        questionID: 7,
         question: 'Who is the all-time leading goal scorer in FIFA World Cup history?',
-        choice1: 'Lionel Messi',
-        choice2: 'Cristiano Ronaldo',
-        choice3: 'Pele',
-        choice4: 'Miroslav Klose',
-        answer: 4,
+        answers: {
+            a: 'Lionel Messi',
+            b: 'Cristiano Ronaldo',
+            c: 'Pele',
+            d: 'Miroslav Klose'
+        },
+        correctAnswer: 'd'
     },
     {
+        questionID: 8,
         question: 'Which country won the first-ever FIFA World Cup in 1930?',
-        choice1: 'Germany',
-        choice2: 'Brazil',
-        choice3: 'Italy',
-        choice4: 'Uruguay',
-        answer: 4,
+        answers: {
+            a: 'Brazil',
+            b: 'Uruguay',
+            c: 'Germany',
+            d: 'Italy'
+        },
+        correctAnswer: 'b'
     },
     {
+        questionID: 9,
         question: 'Which country won the 2022 FIFA World Cup?',
-        choice1: 'France',
-        choice2: 'Brazil',
-        choice3: 'Argentina',
-        choice4: 'Spain',
-        answer: 3,
-    },
-]
+        answers: {
+            a: 'France',
+            b: 'Brazil',
+            c: 'Argentina ',
+            d: 'Spain'
+        },
+        correctAnswer: 'c'
+    }
+];
 
 let currentExhibit = 0;
 let exhibitCounter = 1;
@@ -216,4 +255,91 @@ function generateQuiz() {
     }
     
     displayQuiz();
+}
+
+/** 
+ * Iterates through the options and adds the class of radio-checked to the option 
+ * the user has clicked on/selected, while removing the radio-checked class
+ * from any previously selected radio button which becomes unchecked by default.
+ * @param questionNo the question the user is on
+ * @param selection the user's selected answer
+ */
+function selectOption(questionNo, selection) {
+    // stores the list of option selections in a variable to iterate through them
+    const optionList = ['a', 'b', 'c', 'd'];
+    for (let i = 0; i <= optionList.length - 1; i++) {
+
+        if (optionList[i] == selection) {
+            const optionChoice = document.getElementById(`label${questionNo}_${selection}`);
+            optionChoice.classList.add('radio-checked');
+        } else {
+            const radioBtn = document.getElementById(`label${questionNo}_${optionList[i]}`);
+            radioBtn.classList.remove('radio-checked');
+        }
+    }
+}
+
+/**
+ * Iterates through all quiz questions and checks if each question has a checked radio button.
+ * @returns true
+ * If a question doesn't have a radio button checked, an error message is displayed.
+ */
+function validateChecked() {
+    
+    let allChecked = true;
+
+    // Calls function to get username from url and assigns it to a variable
+    let user = saveUserName();
+
+    for (let i = 0; i <= questions.length - 1; i++) {
+        
+        let name = `question${[i]}`;
+        // if radio button is unchecked (returns an empty string and posts null),
+        // then sets allChecked to false
+        if (document.querySelector(`input[name='${name}']:checked`) == null) {
+            allChecked = false;
+        }
+    }
+
+    if (allChecked == false) {
+        alertMsg.innerHTML = `Hey ${user}, did you answer all questions?`;
+
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * If validateChecked function returns true, loops through all the questions and checks 
+ * their value against the correct answer, if correct it increments the score by 1 and
+ * calls the displayResults function taking the total score as a parameter.
+ */
+function getResults() {
+
+    if (validateChecked()) {
+
+        let correctTotal = 0;
+        for (let i = 0; i <= questions.length - 1; i++) {
+            
+            // assigns a variable to the correct answer for each current question
+            const correctAns = questions[i].correctAnswer;
+            
+            // assigns a variable to the question ID of each current question
+            const questionID = questions[i].questionID;
+            
+            let name = `question${questionID}`;
+            // stores the value of the selected radio button to the variable
+            const selectedAnswer = document.querySelector(`input[name='${name}']:checked`).value;
+
+            // checks if value of selected answer is equal to the correct answer
+            if (selectedAnswer == correctAns)
+            {
+                // increments the total by 1 
+                correctTotal += 1;
+            }
+        }
+
+        displayResults(correctTotal);
+    }
 }
